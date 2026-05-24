e62c7a702a4c3074c0f90af0d1383d8fab1a2ab0
Duke2824 <sinkers_toucan3r@icloud.com>
2026-05-24 23:43:47 +0700

    initiate project

---
+++ SimpleGuitarTuner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleGuitarTuner
+//
+//  Created by Nguyen Duc Anh on 2026/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleGuitarTuner/SimpleGuitarTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGuitarTunerApp.swift
+//  SimpleGuitarTuner
+//
+//  Created by Nguyen Duc Anh on 2026/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGuitarTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
